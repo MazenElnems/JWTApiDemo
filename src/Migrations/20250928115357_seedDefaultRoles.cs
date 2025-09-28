822f3efc32b2274ddd8b6bd02e3f5ff2146c9797
Mazen Elnems <mazenelnems22@gmail.com>
2025-09-28 14:57:06 +0300

    seed default roles to the database

---
+++ src/Migrations/20250928115357_seedDefaultRoles.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JWTAuthApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedDefaultRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
+                values: new object[,]
+                {
+                    { Guid.NewGuid(), "User", "USER", Guid.NewGuid().ToString() },
+                    { Guid.NewGuid(), "Admin", "ADMIN", Guid.NewGuid().ToString() }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM AspNetRoles");
+        }
+    }
+}
